528f7e858eb8f2cc621866192de7d6248256891d
Asgar Y. Asgarov <asgaryas@code.local>
2023-01-24 09:21:10 -0800

    uncompleted

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Fiorello.Data;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Slider>sliders = await _context.Sliders.Where(m=>!m.IsDeleted).ToListAsync();
+            return View(sliders);
+        }
+
+       
+    }
+}
